c887ab81ddfd41184c5fa671524f46d9c098292b
Igor Faria <igorfaria@discente.ufg.br>
2021-07-05 10:39:10 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/igorf/wrkmongodb/WrkmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.igorf.wrkmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrkmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrkmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorf/wrkmongodb/WrkmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorf.wrkmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrkmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
