eb370844ffe536b815e8d905e826153eb7486d8b
Igor Faria <igorfaria@discente.ufg.br>
2021-07-05 15:59:39 -0300

    obtendo usuario por id, com ExceptionHandler

--- src/main/java/com/igorf/wrkmongodb/services/UserService.java
+++ src/main/java/com/igorf/wrkmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.igorf.wrkmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.igorf.wrkmongodb.domain.User;
 import com.igorf.wrkmongodb.repository.UserRepository;
+import com.igorf.wrkmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
-		
+	}	
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
---
+++ src/main/java/com/igorf/wrkmongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.igorf.wrkmongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
