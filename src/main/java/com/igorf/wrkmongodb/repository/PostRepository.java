ac8d66a74472247fdf2c4e61c38f3715415e015f
Igor Faria <igorfaria@discente.ufg.br>
2021-07-07 15:23:31 -0300

    consulta simples com @query

--- src/main/java/com/igorf/wrkmongodb/repository/PostRepository.java
+++ src/main/java/com/igorf/wrkmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.igorf.wrkmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.igorf.wrkmongodb.domain.Post;
@@ -10,6 +11,9 @@ import com.igorf.wrkmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
  
