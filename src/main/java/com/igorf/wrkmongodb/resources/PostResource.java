f433f2e696ed1a11c5a82935267c3b2cf5bdca7c
Igor Faria <igorfaria@discente.ufg.br>
2021-07-07 16:15:03 -0300

    consulta com varios criterios

--- src/main/java/com/igorf/wrkmongodb/resources/PostResource.java
+++ src/main/java/com/igorf/wrkmongodb/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.igorf.wrkmongodb.resources;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -43,6 +44,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate
+			){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 	
